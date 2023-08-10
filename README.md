a6bbe0bb6cde4fd6819ffea565671cf4fdd7ae0f
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-08-10 22:14:55 +0700

    create react app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # movies-explorer-frontend
+## Ссылки на проект
+
+IP 51.250.110.109
+
+Frontend https://isildurrr1-diplom.nomoreparties.co
+
+Backend https://api.isildurrr1-diplom.nomoreparties.co
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+import './Promo.css'
+import practicum_logo from '../../images/practicum_logo.svg'
+
+const Promo = () => {
+  return (
+    <div className="promo">
+      <Header />
+      <img className="promo__image" src={practicum_logo} alt="practicum_logo" />
+    </div>
+  );
+};
+
+export default Promo;
