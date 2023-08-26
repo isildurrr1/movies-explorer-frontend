71dddc3dd8fd9b1d7061591e559c60fdff5ba30d
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-08-26 09:43:31 +0700

    add button

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,12 +1,22 @@
 import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 import movies from '../../utils/movies-data'
+import { useState } from 'react';
 
 const MoviesCardList = () => {
+  const [count, setCount] = useState(12);
+  const addMovies = () => setCount(count + 12);
   return (
-    <div className='card-list'>
-      {movies.map((data, i) => <MoviesCard movie={data} key={i} />)}
-    </div>
+    <>
+      <div className='card-list'>
+        {movies.slice(0, count).map((data, i) => <MoviesCard movie={data} key={i} />)}
+      </div>
+      {count < movies.length &&
+        <div className='card-list__btn-container'>
+          <button className='card-list__button' onClick={addMovies}>Ещё</button>
+        </div>
+      }
+    </>
   )
 }
 
