17091ae23d008f2038f635cb9902047bf3e9a9ed
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-09-07 15:31:44 +0700

    add shorts & localstorage

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,39 +1,57 @@
 import { useEffect, useState } from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
 import Search from '../Search/Search';
 import './Movies.css';
 import { moviesApi } from '../../utils/MoviesApi';
 
 const Movies = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(localStorage.getItem('searchQuery'));
+  const [shortActive, setShortActive] = useState(JSON.parse(localStorage.getItem('shortActive')));
 
   const handleSearch = (searchString) => setSearchQuery(searchString);
+  const handleShortFilms = () => setShortActive(!shortActive)
 
-  const searchMovieFilter = (allMovies, searchQueryStr) => {
+  const searchMovieFilter = (allMovies, searchQueryStr, shortActive) => {
     const reg = new RegExp(`${searchQueryStr}`, 'i')
-    return allMovies.filter(function (movie) {
+    let filtredMovies = allMovies.filter(function (movie) {
       return reg.test(movie.nameRU);
     })
+    if (!shortActive) {
+      return filtredMovies.filter(function (movie) {
+        return movie.duration >= 40;
+      })
+    }
+    return filtredMovies;
   }
 
   useEffect(() => {
+    if (shortActive === null) setShortActive(true)
+    localStorage.setItem('shortActive', shortActive)
     if (searchQuery) {
+      setLoading(true)
+      localStorage.setItem('searchQuery', searchQuery)
       moviesApi.getMovies()
         .then((res) => {
-          setMovies(searchMovieFilter(res, searchQuery));
-          setSearchQuery('');
+          setMovies(searchMovieFilter(res, searchQuery, shortActive));
+          setLoading(false);
         })
         .catch(err => console.log(err))
     }
-  }, [searchQuery]);
-
+  }, [searchQuery, shortActive]);
 
 
   return (
     <main className='movies'>
-      <Search handleSearch={handleSearch} />
-      <MoviesCardList movies={movies} loading={searchQuery} />
+      <Search
+        handleSearch={handleSearch}
+        searchQuery={searchQuery}
+        shortFilmsActive={shortActive}
+        handleShortFilms={handleShortFilms}
+      />
+      <MoviesCardList movies={movies} loading={loading} />
     </main>
   )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './Search.css';
 
-const Search = ({ handleSearch }) => {
-  const [inputValue, setInputValue] = useState('');
-
+const Search = ({ handleSearch, searchQuery, shortFilmsActive, handleShortFilms }) => {
+  const [inputValue, setInputValue] = useState(searchQuery);
   const handleChange = (e) => setInputValue(e.target.value);
+  const handleShort = () => {
+    handleShortFilms()
+  }
 
   const searchSubmit = (e) => {
     e.preventDefault();
@@ -22,13 +24,14 @@ const Search = ({ handleSearch }) => {
             onFocus={(e) => e.target.placeholder = ""}
             onBlur={(e) => e.target.placeholder = "Фильм"}
             onChange={handleChange}
+            value={inputValue}
           />
           <button className='search__button' type='submit'>Найти</button>
         </form>
         <div className="search__switchContainer">
           <label className="search__switch">
             <label htmlFor="checkbox-switch"></label>
-            <input className="search__checkbox" type="checkbox" id="checkbox-switch" defaultChecked />
+            <input className="search__checkbox" type="checkbox" id="checkbox-switch" checked={shortFilmsActive} onChange={handleShort} />
             <span className="search__slider" />
           </label>
           <label htmlFor="checkbox-switch" className="search__switchName">Короткометражки</label>
@@ -37,7 +40,7 @@ const Search = ({ handleSearch }) => {
       <div className="search__switchContainer search__switchContainer_mobile">
         <label className="search__switch">
           <label htmlFor="checkbox-mobile"></label>
-          <input className="search__checkbox" type="checkbox" id="checkbox-mobile" defaultChecked />
+          <input className="search__checkbox" type="checkbox" id="checkbox-mobile" checked={shortFilmsActive} onChange={handleShort} />
           <span className="search__slider" />
         </label>
         <label htmlFor="checkbox-mobile" className="search__switchName">Короткометражки</label>
