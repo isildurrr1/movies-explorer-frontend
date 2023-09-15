fb62cd82c1f70f5a6e9698f390aec21b7db77782
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-09-15 19:30:32 +0700

    1st iteration

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,9 +1,9 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 import { mainApi } from '../../utils/MainApi';
 import { useEffect, useState } from 'react';
 
-const MoviesCard = ({ movie, myMovies, setHideCard}) => {
+const MoviesCard = ({ movie, myMovies, setHideCard }) => {
 
   const location = useLocation();
   const [saved, setSaved] = useState(true)
@@ -35,7 +35,9 @@ const MoviesCard = ({ movie, myMovies, setHideCard}) => {
     if (saved) {
       mainApi.deleteMyMovie(id)
         .then(() => {
-          setHideCard(true)
+          if (location.pathname !== '/movies') {
+            setHideCard(true)
+          }
           setSaved(false)
         })
         .catch(err => console.log(err))
@@ -53,17 +55,19 @@ const MoviesCard = ({ movie, myMovies, setHideCard}) => {
           className="moviesCard__save-btn"
           onClick={saveMovie}
         >Сохранить</button>}
-      <img src={`${location.pathname === '/movies' ? `https://api.nomoreparties.co/${movie.image.url}` : movie.image}`}
-        alt="Обложка"
-        className="moviesCard__frame" />
-      <div className="moviesCard__discription">
-        <h2 className="moviesCard__name">{movie.nameRU}</h2>
-        <p className="moviesCard__duration">{
-          movie.duration > 60 ?
-            `${Math.floor(movie.duration / 60)}ч ${movie.duration % 60}м`
-            : `${movie.duration}м`
-        }</p>
-      </div>
+      <Link to={movie.trailerLink} className="moviesCard__link" target='blank'>
+        <img src={`${location.pathname === '/movies' ? `https://api.nomoreparties.co/${movie.image.url}` : movie.image}`}
+          alt="Обложка"
+          className="moviesCard__frame" />
+        <div className="moviesCard__discription">
+          <h2 className="moviesCard__name">{movie.nameRU}</h2>
+          <p className="moviesCard__duration">{
+            movie.duration > 60 ?
+              `${Math.floor(movie.duration / 60)}ч ${movie.duration % 60}м`
+              : `${movie.duration}м`
+          }</p>
+        </div>
+      </Link>
     </article>
   )
 }
